071ab5b776ad90f91b05ce32c6561dff862a29c4
zhi79928-ui <zhi79928@gmail.com>
2026-09-21 12:03:32 +0800

    chore: configure habit reminder template

--- config/reminders.js
+++ config/reminders.js
@@ -1,2 +1,5 @@
 // 云函数、数据库权限、模板、定时触发器均完成部署并真机验证后才设为 true。
-module.exports = { enabled: false, templateId: '' };
+module.exports = {
+  enabled: false,
+  templateId: 'jOxydcsFmwAJGoJTUqNvCoYrjlJRDd9_b8ZpU77GVqc'
+};
